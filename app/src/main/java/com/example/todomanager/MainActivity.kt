b014315fbb9635bee99f6bc6b599499bf4921e35
sardor <sardor@sunweb.pro>
2021-01-18 01:58:57 +0500

    activity logic

--- app/src/main/java/com/example/todomanager/MainActivity.kt
+++ app/src/main/java/com/example/todomanager/MainActivity.kt
@@ -1,11 +1,116 @@
 package com.example.todomanager
 
+import android.content.Intent
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import android.widget.ListView
+import androidx.annotation.RequiresApi
+import com.example.kotlinhell.TaskDto
+import com.google.firebase.database.*
+import java.text.SimpleDateFormat
+import java.util.*
+import kotlin.collections.ArrayList
+import kotlin.collections.HashMap
 
 class MainActivity : AppCompatActivity() {
+    lateinit var adapter: TaskAdapter
+    private var listViewItems: ListView? = null
+    lateinit var database: DatabaseReference
+    var mappedItemList: ArrayList<TaskDto> = ArrayList()
+    @RequiresApi(Build.VERSION_CODES.O)
+
+    var itemListener: ValueEventListener = object : ValueEventListener {
+        @RequiresApi(Build.VERSION_CODES.O)
+        override fun onDataChange(dataSnapshot: DataSnapshot) {
+            // Get Post object and use the values to update the UI
+            addDataToList(dataSnapshot)
+        }
+        override fun onCancelled(databaseError: DatabaseError) {
+            // Getting Item failed, log a message
+            Log.w("MainActivity", "loadItem:onCancelled", databaseError.toException())
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        this.database = FirebaseDatabase.getInstance(Constants.DATABASE_URL).reference
+        this.adapter = TaskAdapter(this, this.mappedItemList)
+        this.listViewItems = findViewById(R.id.lvItems)
+        this.listViewItems!!.adapter = this.adapter
+
+        this.database.orderByKey().addListenerForSingleValueEvent(itemListener)
+
+        val sdf = SimpleDateFormat(Constants.DATE_FORMAT, Locale.US)
+        this.listViewItems!!.setOnItemClickListener { parent, view, position, id ->
+            val intent = Intent(this, TaskCard::class.java)
+            val selectedTask = this.mappedItemList[position]
+            if (!selectedTask.done) {
+                intent.putExtra("taskTitle", selectedTask.title)
+                intent.putExtra("taskDescription", selectedTask.description)
+                intent.putExtra("taskDeadline", sdf.format(selectedTask.deadline))
+                intent.putExtra("taskId", selectedTask.objectId)
+                this.startActivity(intent)
+            }
+        }
+    }
+
+    fun createNewRecord(view: View){
+        val intent = Intent(this, NewTask::class.java)
+        startActivity(intent)
+    }
+
+
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun addDataToList(dataSnapshot: DataSnapshot) {
+        val items = dataSnapshot.children.iterator()
+        //Check if current database contains any collection
+        if (items.hasNext()) {
+            val toDoListindex = items.next()
+            val itemsIterator = toDoListindex.children.iterator()
+
+            //check if the collection has any to do items or not
+            while (itemsIterator.hasNext()) {
+                //get current item
+                val currentItem = itemsIterator.next()
+                val todoItem = Task.create()
+                //get current data in a map
+                val map = currentItem.getValue() as HashMap<*, *>
+                //key will return Firebase ID
+                todoItem.objectId = currentItem.key
+                todoItem.title = map.get("title") as String?
+                todoItem.description = map.get("description") as String?
+                todoItem.deadline = map.get("deadline") as String
+                todoItem.done = map.get("done") as Boolean
+                this.mappedItemList.add(TaskDto(todoItem))
+            }
+        }
+        Collections.sort(
+            this.mappedItemList,
+            Comparator.comparing(TaskDto::done)
+                .thenComparing(TaskDto::deadline)
+        )
+        //alert adapter that has changed
+        adapter.notifyDataSetChanged()
+    }
+
+    fun modifyItemState(itemObjectId: String, isDone: Boolean) {
+        val itemReference = this.database.child(Constants.FIREBASE_ITEM).child(itemObjectId)
+        itemReference.child("done").setValue(isDone);
+    }
+
+    //delete an item
+    fun onItemDelete(itemObjectId: String) {
+        //get child reference in database via the ObjectID
+        val itemReference = this.database.child(Constants.FIREBASE_ITEM).child(itemObjectId)
+        //deletion can be done via removeValue() method
+        itemReference.removeValue()
     }
 }
--- app/src/main/java/com/example/todomanager/NewTask.kt
+++ app/src/main/java/com/example/todomanager/NewTask.kt
@@ -1,11 +1,91 @@
 package com.example.todomanager
 
+import android.app.DatePickerDialog
+import android.content.Intent
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AlertDialog
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import java.text.SimpleDateFormat
+import java.util.*
+import kotlinx.android.synthetic.main.activity_new_task.*
+import java.time.format.DateTimeFormatter
 
 class NewTask : AppCompatActivity() {
+    lateinit var database: DatabaseReference
+    @RequiresApi(Build.VERSION_CODES.O)
+    val dateFormatter = DateTimeFormatter.ofPattern(Constants.DATE_FORMAT, Locale.US)
+    lateinit var isoDate: String
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_new_task)
+        setTitle("Добавить")
+
+        this.database = FirebaseDatabase.getInstance(Constants.DATABASE_URL).reference
+
+        bindCalendarDialogToTextInput()
+
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    fun addTask(view: View) {
+
+        val newTaskTitle = TaskNameInput.text.toString()
+        val newTaskDescription = DescriptionInput.text.toString()
+        val newTaskDeadline = EndDateInput.text.toString()
+
+        if (newTaskTitle == "" || newTaskDeadline == "") {
+            val alert = AlertDialog.Builder(this).setPositiveButton("Ok") { d, id->d.cancel() }
+            alert.setMessage("Введите обязательные поля").create()
+            alert.show()
+        }
+        else {
+            val task = Task.create()
+            task.title = newTaskTitle
+            task.description = newTaskDescription
+            task.deadline = this.isoDate
+            task.done = false
+
+            val createdTask = this.database.child(Constants.FIREBASE_ITEM).push()
+            task.objectId = createdTask.key
+            createdTask.setValue(task)
+
+            val intent = Intent(this, MainActivity::class.java)
+            this.startActivity(intent)
+        }
+    }
+
+    fun bindCalendarDialogToTextInput() {
+        val calendar = Calendar.getInstance()
+
+        val dateSetListener = DatePickerDialog.OnDateSetListener { view, year, monthOfYear, dayOfMonth ->
+            calendar.set(Calendar.YEAR, year)
+            calendar.set(Calendar.MONTH, monthOfYear)
+            calendar.set(Calendar.DAY_OF_MONTH, dayOfMonth)
+
+            val sdf = SimpleDateFormat(Constants.DATE_FORMAT, Locale.US)
+            val isoSdf = SimpleDateFormat(Constants.ISO_FORMAT, Locale.US)
+            EndDateInput.setText(sdf.format(calendar.time))
+            this.isoDate = isoSdf.format(calendar.time)
+        }
+
+        EndDateInput.setOnClickListener {
+            DatePickerDialog(
+                this,
+                dateSetListener,
+                calendar.get(Calendar.YEAR),
+                calendar.get(Calendar.MONTH),
+                calendar.get(Calendar.DAY_OF_MONTH)
+            ).show()
+        }
     }
 }
