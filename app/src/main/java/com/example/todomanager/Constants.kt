400b7d573db11564a404507da548dfdc64fa73d5
sardor <sardor@sunweb.pro>
2021-01-18 01:59:12 +0500

    constants

---
+++ app/src/main/java/com/example/todomanager/Constants.kt
@@ -0,0 +1,10 @@
+package com.example.todomanager
+
+object Constants {
+    @kotlin.jvm.JvmField
+    var DATE_FORMAT_JAVA: String = "dd.MM.yyyy"
+    @JvmStatic val ISO_FORMAT: String = "yyyy-MM-dd"
+    @JvmStatic val FIREBASE_ITEM: String = "task"
+    @JvmStatic val DATABASE_URL: String = "https://todo-manager-40651-default-rtdb.firebaseio.com/"
+    @JvmStatic val DATE_FORMAT: String = "dd.MM.yyyy"
+}
